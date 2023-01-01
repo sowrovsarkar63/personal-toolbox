761b7e95eb94ed86b82d7b54af629c684e28f930
Md sowrov sarkar <dev.sowrovsarkar63@gmail.com>
2023-01-01 21:49:38 +0600

    Project file structure created with webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,117 @@
+// Webpack uses this to work with directories
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// This is the main configuration object.
+// Here, you write different options and tell Webpack what to do
+module.exports = {
+  // Path to your entry point. From this file Webpack will begin its work
+  entry: {
+    index: "./src/js/index.js",
+  },
+
+  devServer: {
+    static: "./dist",
+  },
+  // optimization: {
+  //   runtimeChunk: "single",
+  // },
+
+  // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+
+  // Set rules for loader and babel transpilar
+  module: {
+    rules: [
+      // apply rules for js file
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+
+      //   another rules for sass
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // After all CSS loaders, we use a plugin to do its work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader",
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader",
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
+            },
+          },
+        ],
+      },
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            // Using file-loader for these files
+            loader: "file-loader",
+
+            // In options we can set different things like format
+            // and directory to save
+            options: {
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+  ],
+
+  // Default mode for Webpack is production.
+  // Depending on mode Webpack will apply different things
+  // on the final bundle. For now, we don't need production's JavaScript
+  // minifying and other things, so let's set mode to development
+  mode: "development",
+};
